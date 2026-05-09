ebd2d1832eed6a7a64de8d4ec955b15878c92e1f
njp2k5 <nikhiljp05@gmail.com>
2026-05-09 22:23:16 +0530

    feat(product-service): persist products in database

---
+++ services/product-service/src/main/java/com/amazonlite/product/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.amazonlite.product.repository;
+
+import com.amazonlite.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+}
